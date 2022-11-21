bd1755570d63b24335b7a06eaaebb82475faf40c
Nikhil Agarwal <nagarwal1701@gmail.com>
2022-11-21 06:09:44 +0530

    array vs arraylist

--- Java-Learnings/src/self/java8/stream/Basics.java
+++ Java-Learnings/src/self/java8/stream/Basics.java
@@ -1,10 +1,24 @@
 package self.java8.stream;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Basics {
 	public static void main(String[] args) {
+		
+		//For arrays (int[]), a data structure, use Arrays.stream or Stream.of
 		Arrays.stream(new int[] {4, 2, 6, 3, 1})
+				.filter(x -> x % 2 != 0)
+				.forEach(x -> System.out.println(x));
+		
+		// For ArrayList, a collection, use variablename.stream()
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(4);
+		list.add(2);
+		list.add(6);
+		list.add(3);
+		list.add(1);
+		list.stream()
 				.filter(x -> x % 2 != 0)
 				.forEach(x -> System.out.println(x));
 	}
