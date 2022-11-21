3b9f505faaad76558f8aff5d02671b2055966576
Nikhil Agarwal <nagarwal1701@gmail.com>
2022-11-21 17:54:21 +0530

    reduce to a single output

--- Java-Learnings/src/self/java8/stream/Basics.java
+++ Java-Learnings/src/self/java8/stream/Basics.java
@@ -2,14 +2,27 @@ package self.java8.stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Basics {
 	public static void main(String[] args) {
+//		arrayStream();
+//		System.err.println();
+//		arrayListStream();
+//		System.out.println();
+//		reduceStreamInteger();
+		reduceStreamDouble();
+	}
+	
+	private static void arrayStream() {
 		
 		//For arrays (int[]), a data structure, use Arrays.stream or Stream.of
 		Arrays.stream(new int[] {4, 2, 6, 3, 1})
 				.filter(x -> x % 2 != 0)
 				.forEach(x -> System.out.println(x));
+	}
+	
+	private static void arrayListStream() {
 		
 		// For ArrayList, a collection, use variablename.stream()
 		ArrayList<Integer> list = new ArrayList<>();
@@ -21,5 +34,20 @@ public class Basics {
 		list.stream()
 				.filter(x -> x % 2 != 0)
 				.forEach(x -> System.out.println(x));
+		
+		for(int x : list) System.out.println(x);
+	}
+	
+	private static void reduceStreamInteger() {
+		
+		Integer total = Stream.of(2, 3, 4, 5)
+								.reduce(0, (Integer a, Integer b) -> a + b);
+		System.out.println(total);
+	}
+	
+	private static void reduceStreamDouble() {
+		double total = Stream.of(2.3, 4.2, 5.3, 5.5).reduce(0.0, (Double a, Double b) -> a + b);
+		System.out.println(total);
+		System.out.println();
 	}
 }
